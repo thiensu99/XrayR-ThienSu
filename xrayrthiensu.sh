c22f45ae3d95114cb0b2e293346a3f4fc3f9d4b4
Thiên Sứ <91358817+thiensu99@users.noreply.github.com>
2022-05-27 15:03:34 +0700

    Update xrayrthiensu.sh

--- xrayrthiensu.sh
+++ xrayrthiensu.sh
@@ -124,8 +124,8 @@ Nodes:
       CertConfig:
         CertMode: $CertMode # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
         CertDomain: "$CertDomain" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert  # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/node1.test.com.key
+        CertFile: /root/cert/server.pem  # Provided if the CertMode is file
+        KeyFile: /root/cert/privkey.pem
         Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
         Email: test@me.com
         DNSEnv: # DNS ENV option used by DNS provider
